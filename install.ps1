8a6d3affddd044816de4fafd4bb7eed1d225df95
Rendani Sinyage <sinyage@gmail.com>
2026-02-14 13:58:25 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -165,6 +165,7 @@ foreach ($wf in $vitalWorkflows) {
     }
 
     # Final normalization to LF and save with UTF8 (No BOM)
+    $content = $content.Trim() + "`n"
     $content = $content -replace "`r`n", "`n"
     $fullDest = [System.IO.Path]::GetFullPath((Join-Path (Get-Location) $dest))
     $dir = [System.IO.Path]::GetDirectoryName($fullDest)
