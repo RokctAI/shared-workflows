c5e8c9b56e50b6c33c10d377f3d91b3c8edd8db3
Rendani Sinyage <sinyage@gmail.com>
2026-05-02 19:02:30 +0200

    Update build_ecosystem.sh

--- scripts/build_ecosystem.sh
+++ scripts/build_ecosystem.sh
@@ -233,6 +233,7 @@ else
     sudo chown -R $CURRENT_USER:$CURRENT_USER /home/frappe/frappe-bench
     sudo chmod -R 777 /home/frappe/frappe-bench/env
     sudo chmod -R 777 /home/frappe/frappe-bench/sites
+    sudo chmod -R 777 /home/frappe/frappe-bench/logs
     
     # Specifically target site-packages for poorly packaged apps like plaid-python
     # Pre-create the directory that causes permission issues during install
